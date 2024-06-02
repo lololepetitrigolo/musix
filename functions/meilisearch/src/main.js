d9060d0abed40d2e16b5f254d4249a9c5de9c8f1
Loïs Guillin <lois.guillin@grenoble-inp.org>
2024-06-02 22:13:03 +0200

    functions try to sync

--- functions/meilisearch/src/main.js
+++ functions/meilisearch/src/main.js
@@ -13,31 +13,31 @@ export default async ({ req, res, log }) => {
     'MEILISEARCH_SEARCH_API_KEY',
   ]);
 
-  if (req.method === 'GET') {
-    const html = interpolate(getStaticFile('index.html'), {
-      MEILISEARCH_ENDPOINT: process.env.MEILISEARCH_ENDPOINT,
-      MEILISEARCH_INDEX_NAME: process.env.MEILISEARCH_INDEX_NAME,
-      MEILISEARCH_SEARCH_API_KEY: process.env.MEILISEARCH_SEARCH_API_KEY,
-    });
+  // if (req.method === 'GET') {
+  //   const html = interpolate(getStaticFile('index.html'), {
+  //     MEILISEARCH_ENDPOINT: process.env.MEILISEARCH_ENDPOINT,
+  //     MEILISEARCH_INDEX_NAME: process.env.MEILISEARCH_INDEX_NAME,
+  //     MEILISEARCH_SEARCH_API_KEY: process.env.MEILISEARCH_SEARCH_API_KEY,
+  //   });
 
-    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' });
-  }
+  //   return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' });
+  // }
 
   const client = new Client()
     .setEndpoint(
-      process.env.APPWRITE_ENDPOINT ?? 'https://cloud.appwrite.io/v1'
+      "https://ef7c-37-165-164-237.ngrok-free.app/v1"
     )
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
+    .setProject("66589d630000a9fbb710")
+    .setKey("7cb3433e157832ff885564694ddf06c3fb8e80da1f842d4917b13eaaae655b8c81d32faf9a872ed4a3df583df1d690c8cb384a256525ae28e3f99660e5155fa35a4d6b18f912e66013c58e6ce5a1cb1870e972fd0bd5a7de7da74340bc37c2fd08b1527aa40af54dd2dc6cfecf18e5683b6b599ad558a5d563b1c2e21cd1d96c");
 
   const databases = new Databases(client);
 
   const meilisearch = new MeiliSearch({
-    host: process.env.MEILISEARCH_ENDPOINT,
-    apiKey: process.env.MEILISEARCH_ADMIN_API_KEY,
+    host: "http://localhost:7700",
+    apiKey: "meilisearchKey",
   });
 
-  const index = meilisearch.index(process.env.MEILISEARCH_INDEX_NAME);
+  const index = meilisearch.index("sounds");
 
   let cursor = null;
 
@@ -49,8 +49,8 @@ export default async ({ req, res, log }) => {
     }
 
     const { documents } = await databases.listDocuments(
-      process.env.APPWRITE_DATABASE_ID,
-      process.env.APPWRITE_COLLECTION_ID,
+      "6658a0760030ec7e3bb4",
+      "6658a0c700292cade378",
       queries
     );
 
